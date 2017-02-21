466fe93f888866a1fe6c62e8be99bb0b50ab69a6
Andriy Derckach <skaywoker@gmail.com>
2017-02-21 22:54:18 +0200

    Created MainActivity class and layout and added to AndroidManifest

---
+++ app/src/main/java/com/example/bloom/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bloom.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Bloom on 21.02.2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
