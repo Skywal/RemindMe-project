1dc8e6c3e6240e599b55cddf846ec9eb60460faa
Andriy Derckach <skaywoker@gmail.com>
2017-02-22 15:50:08 +0200

    Added toolbar and search button

--- app/src/main/java/com/example/bloom/remindme/MainActivity.java
+++ app/src/main/java/com/example/bloom/remindme/MainActivity.java
@@ -2,15 +2,38 @@ package com.example.bloom.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Bloom on 21.02.2017.
  */
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+    /**ініціалізація і конфігурація тулбару*/
+    private void initToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        /**для розтановки обробників подій по натисненню по ітему*/
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        /**вказує що для тулбару потрібно використовувати таке-то меню*/
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
